a799954df1d34c487e602f5918c8357a5f77e2ea
liam <lgh@e.email>
2023-09-27 17:22:34 +0200

    readd to matrix for ZXDiagram

--- src/to_eincode.jl
+++ src/to_eincode.jl
@@ -1,4 +1,6 @@
 Base.Matrix(zxwd::ZXWDiagram) = to_matrix(zxwd)
+Base.Matrix(zxd::ZXDiagram) = to_matrix(zxd)
+Base.Matrix(zxg::ZXGraph) = to_matrix(zxg)
 
 function to_matrix(zxwd::ZXWDiagram; optimizer = GreedyMethod(), verbose = false)
     ec, ts = to_eincode(zxwd)
@@ -7,6 +9,18 @@ function to_matrix(zxwd::ZXWDiagram; optimizer = GreedyMethod(), verbose = false
     verbose && println("Contracting...")
     m = ec_opt(ts...)
     return reshape(m, (1 << nin(zxwd), 1 << nout(zxwd)))
+end
+
+to_matrix(zxg::ZXGraph; kwargs...) = to_matrix(ZXDiagram(zxg); kwargs...)
+function to_matrix(zxd::ZXDiagram; optimizer = GreedyMethod(), verbose = false)
+    nin = sum([(spider_type(zxd, v) in (SpiderType.In, SpiderType.Out)) for v in get_inputs(zxd)])
+    nout = sum([(spider_type(zxd, v) in (SpiderType.In, SpiderType.Out)) for v in get_outputs(zxd)])
+    ec, ts = to_eincode(zxd)
+    verbose && println("Optimizing contraction orders...")
+    ec_opt = optimize_code(ec, uniformsize(ec, 2), optimizer);
+    verbose && println("Contracting...")
+    m = ec_opt(ts...)
+    return reshape(m, (1<<nout, 1<<nin))
 end
 
 function to_eincode(zxwd::ZXWDiagram{T,P}) where {T,P}
@@ -114,3 +128,58 @@ function d_tensor(n::Int)
     n == 2 || error("A D-box can only has arity 2")
     return ComplexF64[1 1; 1 0] # D = T * X
 end
+
+### Can I merge this with the code above?
+to_eincode(zxg::ZXGraph) = to_eincode(ZXDiagram(zxg))
+function to_eincode(zxd::ZXDiagram{T, P}) where {T, P}
+    vs = spiders(zxd)
+    tensors = []
+    ixs = Vector{Tuple{T, T, T}}[]
+    iy = Tuple{T, T, T}[]
+    for v in vs
+        if !(spider_type(zxd, v) in (SpiderType.In, SpiderType.Out))
+            push!(ixs, to_eincode_indices(zxd, v))
+            push!(tensors, to_eincode_tensor(zxd, v))
+        end
+    end
+    for o in get_outputs(zxd)
+        if spider_type(zxd, o) in (SpiderType.In, SpiderType.Out)
+            push!(iy, to_eincode_indices(zxd, o)[])
+        end
+    end
+    for i in get_inputs(zxd)
+        if spider_type(zxd, i) in (SpiderType.In, SpiderType.Out)
+            push!(iy, to_eincode_indices(zxd, i)[])
+        end
+    end
+    s = unwrap_scalar(scalar(zxd))
+    scalar_tensor = zeros(ComplexF64, ())
+    scalar_tensor[] = s
+    push!(ixs, [])
+    push!(tensors, scalar_tensor)
+    return EinCode(ixs, iy), tensors
+end
+
+function to_eincode_indices(zxd::ZXDiagram{T, P}, v) where {T, P}
+    nbs = neighbors(zxd, v; count_mul = true)
+    ids = Tuple{T, T, T}[]
+    isempty(nbs) && return ids
+    curr_nb = nbs[1]
+    curr_mul = 1
+    for i = 1:length(nbs)
+        nb = nbs[i]
+        if nb != curr_nb            
+            curr_nb = nb
+            curr_mul = 1
+        end
+        if nb == v
+            push!(ids, edge_index(v, nb, (curr_mul+1)÷2))
+        else
+            push!(ids, edge_index(v, nb, curr_mul))
+        end
+        curr_mul += 1
+    end
+    return ids
+end
+
+to_eincode_tensor(zxd::ZXDiagram, v) = to_eincode_tensor(spider_type(zxd, v), degree(zxd, v), phase(zxd, v))
